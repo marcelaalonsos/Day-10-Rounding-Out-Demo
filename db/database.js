7d37cd308751ed4946e2043d89df9b705a162929
marcela <marcela.alonso.s@gmail.com>
2022-11-21 21:36:20 -0500

    connected to db

--- db/database.js
+++ db/database.js
@@ -1,4 +1,12 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/hogwarts', { logging: false });
+const db = new Sequelize({
+  host: 'localhost',
+  port: 5432,
+  database: 'hogwarts',
+  dialect: 'postgres',
+  username: 'postgres',
+  password: 'admin',
+  logging: false
+});
 
 module.exports = db;
